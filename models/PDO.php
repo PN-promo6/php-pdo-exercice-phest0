09d74402d8a54527212f1680a4aea9ade61b9f6e
phest0 <jpcid@hotmail.fr>
2020-05-05 17:13:10 +0200

    features: search + comments

--- models/PDO.php
+++ models/PDO.php
@@ -1,6 +1,6 @@
 <?php
-$db_user = "equesada";
-$db_passwd = "equesada";
+$db_user = "Phesto";
+$db_passwd = "admin";
 $db_host = "localhost";
 $db_port = "3306";
 $db_name = "tsn";
--- views/DisplayPosts.php
+++ views/DisplayPosts.php
@@ -19,16 +19,24 @@
       <img src="img/icon.png" width="30" height="30" class="d-inline-block align-top" alt="">
       Tiny Social Network
     </a>
+
+    <form class="form-inline my-2 my-lg-0" method="get">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
       </ul>
+
       <ul class="navbar-nav">
         <li class="nav-item"><a class="nav-link" href="#" role="button">Login</a></li>
         <li class="nav-item"><a class="nav-link" href="#" role="button">Sign Up</a></li>
       </ul>
+
     </div>
   </header>
 
